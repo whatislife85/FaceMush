e229a6315fd7f4294b67933b07e4bc818de574d2
whatislife85 <35640895+whatislife85@users.noreply.github.com>
2018-01-20 19:26:39 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 
-var pics = [['accent_t_copy1.jpg', 1400, 1], ['accord_t.jpg', 1400, 1], ['agria_t_copy1.jpg', 1400, 1], ['almera_t_copy1.jpg', 1400, 1], ['ambo_t_copy4.jpg', 1400, 1], ['amora_t.jpg', 1400, 1], ['amorosa_t.jpg', 1400, 1], ['annabelle_t.jpg', 1400, 1], ['anna_t.jpg', 1400, 1], ['anna_t_copy1.jpg', 1400, 1], ['anya_t_copy1.jpg', 1400, 1], ['argos_t_copy3.jpg', 1400, 1], ['arranbanner_t.jpg', 1400, 1], ['arrancomet_t.jpg', 1400, 1], ['arranpilot_t_copy9.jpg', 1400, 1], ['arranvictory2_t.jpg', 1400, 1], ['arrow_t.jpg', 1400, 1], ['asterix_t.jpg', 1400, 1], ['atlantic_t_copy2.jpg', 1400, 1], ['avalanche_t.jpg', 1400, 1], ['avondale_t_copy1.jpg', 1400, 1], ['axona_t.jpg', 1400, 1], ['ballydoon_t.jpg', 1400, 1], ['balmoral.jpg', 1400, 1], ['bambino_t_copy2.jpg', 1400, 1], ['banba_t_copy1.jpg', 1400, 1], ['barna_t_copy1.jpg', 1400, 1], ['belledefontenay_t.jpg', 1400, 1], ['bluebelle_t.jpg', 1400, 1], ['bluedanube_t.jpg', 1400, 1], ['bonnie_t_copy2.jpg', 1400, 1], ['britishqueen_t_copy2.jpg', 1400, 1], ['brooke_t.jpg', 1400, 1], ['burren_t_copy2.jpg', 1400, 1], ['cabaret_t_copy2.jpg', 1400, 1], ['caesar_t.jpg', 1400, 1], ['camelot_t.jpg', 1400, 1], ['cara_t_copy2.jpg', 1400, 1], ['carlingford_t_copy1.jpg', 1400, 1], ['carnaval2_t.jpg', 1400, 1], ['casablanca_t.jpg', 1400, 1], ['catriona_t.jpg', 1400, 1], ['celine_t_copy2.jpg', 1400, 1], ['charlemont_t.jpg', 1400, 1], ['charlotte_t_copy2.jpg', 1400, 1], ['chincha_t.jpg', 1400, 1], ['cosmos_t_copy2.jpg', 1400, 1], ['dunbarrover_t.jpg', 1400, 1], ['dunbarstandard_t.jpg', 1400, 1], ['dundrod_t.jpg', 1400, 1], ['dunluce_t.jpg', 1400, 1], ['heh.jpg', 1400, 1], ['incadawn_t.jpg', 1400, 1], ['Jonathan Kozol.jpg', 1400, 1], ['madeleine_t.jpg', 1400, 1], ['madingley_t.jpg', 1400, 1], ['marispeer2_t.jpg', 1400, 1], ['markies_t_copy1.jpg', 1400, 1], ['rangerrussett_t.jpg', 1400, 1], ['robinta_t.jpg', 1400, 1], ['romano_t_copy2.jpg', 1400, 1], ['russetburbank_t_copy2.jpg', 1400, 1], ['Some Random Guy.jpg', 1400, 1], ['The Best Linear Algebra Textbook I have Ever Read.jpg', 1400, 1]];
+
+var pics = [['accent_t_copy1.jpg', 1400, 1], ['accord_t.jpg', 1400, 1], ['agria_t_copy1.jpg', 1400, 1], ['almera_t_copy1.jpg', 1400, 1], ['ambo_t_copy4.jpg', 1400, 1], ['amora_t.jpg', 1400, 1], ['amorosa_t.jpg', 1400, 1], ['annabelle_t.jpg', 1400, 1], ['anna_t.jpg', 1400, 1], ['anna_t_copy1.jpg', 1400, 1], ['anya_t_copy1.jpg', 1400, 1], ['argos_t_copy3.jpg', 1400, 1], ['arranbanner_t.jpg', 1400, 1], ['arrancomet_t.jpg', 1400, 1], ['arranpilot_t_copy9.jpg', 1400, 1], ['arranvictory2_t.jpg', 1400, 1], ['arrow_t.jpg', 1400, 1], ['asterix_t.jpg', 1400, 1], ['atlantic_t_copy2.jpg', 1400, 1], ['avalanche_t.jpg', 1400, 1], ['avondale_t_copy1.jpg', 1400, 1], ['axona_t.jpg', 1400, 1], ['ballydoon_t.jpg', 1400, 1], ['balmoral.jpg', 1400, 1], ['bambino_t_copy2.jpg', 1400, 1], ['banba_t_copy1.jpg', 1400, 1], ['barna_t_copy1.jpg', 1400, 1], ['belledefontenay_t.jpg', 1400, 1], ['bluebelle_t.jpg', 1400, 1], ['bluedanube_t.jpg', 1400, 1], ['bonnie_t_copy2.jpg', 1400, 1], ['britishqueen_t_copy2.jpg', 1400, 1], ['brooke_t.jpg', 1400, 1], ['burren_t_copy2.jpg', 1400, 1], ['cabaret_t_copy2.jpg', 1400, 1], ['caesar_t.jpg', 1400, 1], ['camelot_t.jpg', 1400, 1], ['cara_t_copy2.jpg', 1400, 1], ['carlingford_t_copy1.jpg', 1400, 1], ['carnaval2_t.jpg', 1400, 1], ['casablanca_t.jpg', 1400, 1], ['catriona_t.jpg', 1400, 1], ['celine_t_copy2.jpg', 1400, 1], ['charlemont_t.jpg', 1400, 1], ['charlotte_t_copy2.jpg', 1400, 1], ['chincha_t.jpg', 1400, 1], ['cosmos_t_copy2.jpg', 1400, 1], ['dunbarrover_t.jpg', 1400, 1], ['dunbarstandard_t.jpg', 1400, 1], ['dundrod_t.jpg', 1400, 1], ['dunluce_t.jpg', 1400, 1], ['heh.jpg', 1400, 1], ['incadawn_t.jpg', 1400, 1], ['Jonathan Kozol.jpg', 1400, 1], ['madeleine_t.jpg', 1400, 1], ['madingley_t.jpg', 1400, 1], ['marispeer2_t.jpg', 1400, 1], ['markies_t_copy1.jpg', 1400, 1], ['rangerrussett_t.jpg', 1400, 1], ['robinta_t.jpg', 1400, 1], ['romano_t_copy2.jpg', 1400, 1], ['russetburbank_t_copy2.jpg', 1400, 1], ['Some Random Guy.jpg', 1400, 1], ['The Best Linear Algebra Textbook I have Ever Read.jpg', 1400, 1]];
 
 // Declaring Variables for hoisting
 
@@ -22,7 +23,7 @@ function start() {
 
 	document.getElementById("rightPic").src = "images/" + pics[Math.floor(Math.random() * pics.length)][0];
 	
-	while(document.getElementById("leftPic").src === document.getElementById("rightPic").src || document.getElementById("leftPic").src === "http://localhost/") {
+	while(document.getElementById("leftPic").src === document.getElementById("rightPic").src || document.getElementById("leftPic").src === "https://whatislife85.github.io/FaceMush/index.html") {
 		document.getElementById("leftPic").src = "images/" + pics[Math.floor(Math.random() * pics.length)][0];
 	}
 }
@@ -38,8 +39,8 @@ function newLeftPic() {
 
 	console.log("RIGHT IMAGE: " + pics[randomNumberLeft][0], "  Ra (LEFT RATING): " + Ra, "  Rb (RIGHT RATING): " + Rb);
 
-	if ("http://localhost/images/" + pics[randomNumberLeft][0] === document.getElementById("leftPic").src
-	|| document.getElementById("rightPic").src === "http://localhost/images/" + pics[randomNumberLeft][0]
+	if ("https://whatislife85.github.io/FaceMush/images/" + pics[randomNumberLeft][0] === document.getElementById("leftPic").src
+	|| document.getElementById("rightPic").src === "https://whatislife85.github.io/FaceMush/images/" + pics[randomNumberLeft][0]
 	//|| Rb < (Ra - 20)
 	) {
 		console.log("OH NO");
@@ -51,7 +52,7 @@ function newLeftPic() {
 		Sa = 0;
 
 		for(i = 0; i < pics.length; i++) {
-			if (document.getElementById("leftPic").src === "http://localhost/images/" + pics[i][0] && Sa === 0) {
+			if (document.getElementById("leftPic").src === "https://whatislife85.github.io/FaceMush/images/" + pics[i][0] && Sa === 0) {
 				randomNumberRight = i;
 		
 		$(document).ready(function() {
@@ -76,8 +77,8 @@ function newRightPic() {
 
 	console.log("LEFT IMAGE: " + pics[randomNumberRight][0], "  Ra (LEFT RATING): " + Ra, "  Rb: (RIGHT RATING)" + Rb);
 
-	if ("http://localhost/images/" + pics[randomNumberRight][0] === document.getElementById("rightPic").src
-	|| document.getElementById("leftPic").src === "http://localhost/images/" + pics[randomNumberRight][0]
+	if ("https://whatislife85.github.io/FaceMush/images/" + pics[randomNumberRight][0] === document.getElementById("rightPic").src
+	|| document.getElementById("leftPic").src === "https://whatislife85.github.io/FaceMush/images/" + pics[randomNumberRight][0]
 	//|| Ra < (Rb - 20)
 	)	{
 		console.log("OH NO");
@@ -91,7 +92,7 @@ function newRightPic() {
 		Sb = 0;
 
 		for(i = 0; i < pics.length; i++) {
-			if (document.getElementById("rightPic").src === "http://localhost/images/" + pics[i][0] && Sb === 0) {
+			if (document.getElementById("rightPic").src === "https://whatislife85.github.io/FaceMush/images/" + pics[i][0] && Sb === 0) {
 				randomNumberLeft = i;
 		$(document).ready(function() {
 			$.ajax({
