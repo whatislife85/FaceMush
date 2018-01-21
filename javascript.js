32c3bf2b24d7deff92c26bf36ac98755592db098
whatislife85 <35640895+whatislife85@users.noreply.github.com>
2018-01-20 19:58:04 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -138,7 +138,7 @@ function test() {
 		console.log(ranking_list);
 		$("ul").html("");
 		for (i = ranking_list.length - 1; i >=0; i--) {
-			var ranking_pic = "http://localhost/images/" + ranking_list[i][1];
+			var ranking_pic = "https://github.com/whatislife85/FaceMush/tree/master/images/" + ranking_list[i][1];
 			$("ul").append("<li><img src = "+ranking_pic+"></img><div>Clicks: "+ranking_list[i][0]+"<br>Image Name: "+ranking_list[i][1]+"<br>Image Elo Ranking: "+ranking_list[i][2]+"</div></li><br><br>");
 		}
 	},
